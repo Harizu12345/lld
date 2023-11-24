ec6786c5d8646bf1306da8c0a250c06ba3d08ef5
msi <msi@DESKTOP-Q39FBF1>
2023-11-24 14:37:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+enum class EntityType {
+    Bird,
+    Livestock,
+    Human
+};
+
+struct Characteristic {
+    double flightSpeed;
+    bool clovenHoofed;
+    int IQLevel;
+};
+
+struct LivingEntity {
+    double movementSpeed;
+    EntityType type;
+    string color;
+};
+
+int main() {
+
+    LivingEntity birdEntity = { 10.5, EntityType::Bird, "Blue" };
+    LivingEntity livestockEntity = { 5.4, EntityType::Livestock, "Brown" };
+    LivingEntity humanEntity = { 46, EntityType::Human, "White or Black" };
+
+
+    cout << "bird speed: " << birdEntity.movementSpeed << " km/h" << "\nColor: " << birdEntity.color << "\n" << endl;
+    cout << "livestock speed: " << livestockEntity.movementSpeed << " km/h" << "\nColor: " << livestockEntity.color << "\n" << endl;
+    cout << "human speed: " << humanEntity.movementSpeed << " km/h" << "\nColor: " << humanEntity.color << "\n" << endl;
+
+    return 0;
+}
